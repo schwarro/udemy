d39d1f45d5647c47eaa20b03a1f2f4407d7c9afa
schwarro <rob.ap.schwartz@gmail.com>
2019-01-28 17:33:03 -0500

    created score keeper with js

---
+++ dom/script.js
@@ -0,0 +1,51 @@
+let p1 = document.querySelector("#p1");
+let p2 = document.querySelector("#p2");
+let reset = document.querySelector("#reset");
+let h1 = document.querySelector('h1');
+let p1Score = document.querySelector("#p1Score");
+let p2Score = document.querySelector("#p2Score");
+let p = document.querySelector('p');
+let numInput = document.querySelector('input');
+
+let p1CurrentScore = 0;
+let p2CurrentScore = 0;
+
+let gameOver = false;
+let win;
+
+numInput.addEventListener("change", function(){
+  p.textContent = "Playing to: " + numInput.value;
+  win = Number(numInput.value);
+})
+
+p1.addEventListener("click", function(){
+  if(!gameOver){
+    p1CurrentScore++;
+    if(p1CurrentScore === win){
+      p1Score.classList.add("winner");
+      gameOver = true;
+    }
+    p1Score.innerHTML = p1CurrentScore;
+  }
+})
+
+p2.addEventListener("click", function(){
+  if(!gameOver){
+    p2CurrentScore++;
+    if(p2CurrentScore === win){
+      p2Score.classList.add("winner");
+      gameOver = true;
+    }
+    p2Score.innerHTML = p2CurrentScore;
+  }
+})
+
+reset.addEventListener("click", function(){
+  p1Score.innerHTML = 0;
+  p2Score.innerHTML = 0;
+  p1Score.classList.remove("winner");
+  p2Score.classList.remove("winner");
+  gameOver = false;
+  p1CurrentScore = 0;
+  p2CurrentScore = 0;
+})
